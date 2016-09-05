1d6d54c5a204cdee3f2a1148b6ef60f539aa63e7
Jared Thomas <jjthom87@yahoo.com>
2016-09-05 16:39:08 -0400

    start

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var exphbs = require('express-handlebars');
+var methodOverride = require('method-override');
+
+var User = require('./User.model');
+
+var db = 'mongodb://localhost/usercomments';
+
+app.use('/static', express.static('public/assets'));
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(methodOverride('_method'));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+var PORT = process.env.PORT || 8000;
+
+app.listen(PORT, function () {
+  console.log('database operation on port: ' + PORT);
+ });
